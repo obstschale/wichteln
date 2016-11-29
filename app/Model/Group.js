c8ede9873eb6a0a7ddfcdff93c0328f481df1e39
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2016-11-29 16:06:34 +0100

    🎉 Init Commit - AdonisJS

---
+++ app/Model/Group.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const Lucid = use('Lucid')
+
+class Group extends Lucid {
+
+}
+
+module.exports = Group
---
+++ app/Model/Member.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const Lucid = use('Lucid')
+
+class Member extends Lucid {
+
+}
+
+module.exports = Member
