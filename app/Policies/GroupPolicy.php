6610cbdfc3a37a0f86c172c8fc0bccc168f551f4
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2016-12-23 18:43:39 +0100

    Add GroupPolicy

---
+++ app/Policies/GroupPolicy.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Group;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class GroupPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view the group.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Group  $group
+     * @return mixed
+     */
+    public function view(User $user, Group $group)
+    {
+        return $user->belongsToGroup($group);
+    }
+
+    /**
+     * Determine whether the user can create groups.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the group.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Group  $group
+     * @return mixed
+     */
+    public function update(User $user, Group $group)
+    {
+        return $user->isAdminInGroup($group);
+    }
+
+    /**
+     * Determine whether the user can delete the group.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Group  $group
+     * @return mixed
+     */
+    public function delete(User $user, Group $group)
+    {
+        return $user->isAdminInGroup($group);
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Group;
+use App\Policies\GroupPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        Group::class => GroupPolicy::class,
     ];
 
     /**
@@ -24,7 +26,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
     }
 }
