e7ba276b10b7cb9bdd0b8789e44ad4815a72859e
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2018-11-26 14:48:01 +0100

    Add Imprint & Data Privacy

---
+++ resources/views/imprint.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+    <section id="landing-page" class="hero is-info is-fullheight">
+        <div class="hero-head">
+            <nav class="navbar">
+                <div class="container">
+                    <div class="navbar-brand">
+                        <a class="navbar-item title" href="/">Wichtel.me</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-6 is-offset-3">
+                    <div class="box">
+                        <div class="field">
+                            <h3 class="title has-text-black">Impressum</h3>
+
+                            <h4 class="subtitle has-text-black">Verantwortlich für den Inhalt der Webseite nach § 55 Abs. 2 RStV</h4>
+                            <p>Hans-Helge Bürger</p>
+                            <p>Jakoberstraße 51</p>
+                            <p>86152 Augsburg</p>
+
+                            <br>
+
+                            <h4 class="subtitle has-text-black">Kontakt:</h4>
+
+                            <p>E-Mail: <a href="mailto:webmaster@hanshelgebuerger.de">webmaster@hanshelgebuerger.de</a></p>
+                            <p>Telefon: 015771583142</p>
+
+                            <h4 class="subtitle has-text-black">Hinweis</h4>
+
+                            <p>Die Informationen auf dieser Webseite wurden nach bestem Wissen und Gewissen zusammengestellt. Gleichwohl birgt die Datenschutz-Grundverordnung (DSGVO) eine Reihe juristischer Unschärfen und/oder Regelungslücken. Die notwendige Rechtssicherheit und -klarheit werden voraussichtlich erst gerichtliche Verfahren auf nationaler Ebene oder vor dem Europäischen Gerichtshof bringen. Daher erheben dieser Flyer und die darin bereitgestellten Inhalte keinen Anspruch auf Rechtsverbindlichkeit, Aktualität und Vollständigkeit.</p>
+
+                            <p>Stand aller Informationen: Mai 2018</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
