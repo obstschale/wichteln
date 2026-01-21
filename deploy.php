7cdb7ff32c4b4817ebae2f26fdc86ba6a3d34f9c
Hans-Helge Bürger <mail@hanshelgebuerger.de>
2026-01-21 21:37:06 +0100

    Update and fix deployer

--- deploy.php
+++ deploy.php
@@ -2,41 +2,43 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
+require 'contrib/npm.php';
 
-// Project name
+// Config
 set('application', 'wichtel.me');
+set('git_ssh_command', 'ssh');
 set('http_user', 'rondra');
-
-// Project repository
 set('repository', 'git@github.com:obstschale/wichteln.git');
 
-// [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
-
-// Shared files/dirs between deploys
 add('shared_files', []);
 add('shared_dirs', []);
-
-// Writable dirs by web server
 add('writable_dirs', []);
-set('allow_anonymous_stats', false);
+
+set('git_tty', true);
 
 // Hosts
 
 host('wichtel_prod')
-    ->stage('production')
+    ->set('labels', ['stage' => 'prod'])
+    ->set('remote_user', 'rondra')
     ->set('deploy_path', '~/html/{{application}}');
 
 // Tasks
-
-task('build', function () {
-    run('cd {{release_path}} && build');
+desc('Build npm assets');
+task('npm:build', function () {
+    run("cd {{release_path}} && {{bin/npm}} run build");
 });
 
-// [Optional] if deploy fails automatically unlock.
+desc('Clear OPcache via HTTP');
+task('opcache:reset', function () {
+    $file = '{{release_path}}/public/opcache-reset.php';
+    run("echo '<?php opcache_reset(); echo \"ok\";' > {$file}");
+    run("curl -s https://{{application}}/opcache-reset.php");
+    run("rm -f {$file}");
+});
+
+// Hooks
 after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-
-before('deploy:symlink', 'artisan:migrate');
-
+after('deploy:vendors', 'npm:install');
+after('npm:install', 'npm:build');
+after('deploy:symlink', 'opcache:reset');
